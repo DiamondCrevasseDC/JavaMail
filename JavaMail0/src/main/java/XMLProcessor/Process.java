f7ae2b77a144cb23ce210f086fa1862b7f675b81
chenkang <chenkang@haodf.com>
2020-06-05 12:06:51 +0800

    beanToXml

--- JavaMail0/src/main/java/XMLProcessor/Process.java
+++ JavaMail0/src/main/java/XMLProcessor/Process.java
@@ -6,10 +6,14 @@ import org.junit.Test;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Process {
 
@@ -18,6 +22,21 @@ public class Process {
         Unmarshaller unmarshaller = context.createUnmarshaller();
         Object object = unmarshaller.unmarshal(new File(xmlPath));
         return object;
+    }
+
+    public static void beanToXml(Object object, String xmlPath, Class<?> load) throws JAXBException, IOException {
+        try(FileWriter writer = new FileWriter(xmlPath)) {
+            JAXBContext context = JAXBContext.newInstance(load);
+            Marshaller marshal = context.createMarshaller();
+            marshal.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            // 隐去报文头的生成, Marshaller.JAXB_FRAGMENT默认为false
+            marshal.setProperty(Marshaller.JAXB_FRAGMENT, true);
+            marshal.marshal(object, System.out);
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
+            marshal.marshal(object, writer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static List<Row> getRowList(String filePath) throws JAXBException, IOException {
@@ -29,13 +48,12 @@ public class Process {
         return rowList;
     }
 
-    public static void main(String[] args){
-        List<Row> rowList = null;
+    public static void main(String[] args) {
+        Row row = new Row("2333333");
+        Rowdata rowdata = new Rowdata(Stream.of(row).collect(Collectors.toList()));
         try {
-            rowList = getRowList("D:\\EMAIL.xml");
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            beanToXml(rowdata, "/Users/leo/rowdata.xml", Rowdata.class);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
